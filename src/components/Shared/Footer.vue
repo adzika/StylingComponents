5e36bcd4065ed4840c59cc039d8be92e40d3357e
Ada <adziklipa@gmail.com>
2018-05-28 18:40:03 +0100

    Initial commit

---
+++ src/components/Shared/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <footer>
+        <h1>All servers are managed here</h1>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
